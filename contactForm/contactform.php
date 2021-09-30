2102e2a9e56b53e62d5f367ec63db173163a93ca
Mfrodriguezp <mildri06@gmail.com>
2021-09-29 21:49:36 -0500

    modified the conditions of validation and your answers

--- contactForm/contactform.php
+++ contactForm/contactform.php
@@ -9,11 +9,6 @@ $email_title = "Email:";
 $message_title = "Message:";
 
 /*Datos del formulario*/
-$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING, FILTER_SANITIZE_SPECIAL_CHARS, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-$message = filter_var($_POST['message'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
 if (!$contact_email_to || $contact_email_to == 'contact@example.com') {
     die('The contact form receiving email address is not configured!');
 }
@@ -21,15 +16,25 @@ if (!isset($contact_email_from)) {
     $contact_email_from = "contactform@" . @preg_replace('/^www\./', '', $_SERVER['SERVER_NAME']);
 }
 
-if (strlen($name) < 3 || is_null($name) || $name = '') {
-    echo "El campo <strong>Nombre</strong> tiene menos de 3 caracteres o se encuentra vacío";
-} elseif (!($email)) {
-    echo "Introduzca un correo válido";
-} elseif (strlen($subject) < 3 || is_null($subject) || $subject = '') {
-    echo "El campo <strong>Asunto</strong> tiene menos de 3 caracteres o se encuentra vacío";
-} elseif (strlen($message) < 3 || is_null($message) || $message = '') {
-    echo "El campo <strong>Mensaje</strong> se encuentra vacío";
-} else {
+if (isset($_POST)) {
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $message = filter_var($_POST['message'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    if (strlen($name) <= 3 || $name = '') {
+        die("El campo <strong> Nombre<strong> tiene menos de 3 caracteres o se encuentra vacío");
+    }
+    if (!($email)) {
+        die("Introduzca un correo válido");
+    }
+    if (strlen($subject) < 3 || $subject = '') {
+        die("El campo <strong>Asunto</strong> tiene menos de 3 caracteres o se encuentra vacío");
+    }
+    if (strlen($message) < 3 || $message = '') {
+        die("El campo <strong>Mensaje</strong> tiene menos de 3 caracteres se encuentra vacío");
+    }
+
     $headers = 'From: ' . $name . ' <' . $contact_email_from . '>' . PHP_EOL;
     $headers .= 'Reply-To: ' . $email . PHP_EOL;
     $headers .= 'MIME-Version: 1.0' . PHP_EOL;
